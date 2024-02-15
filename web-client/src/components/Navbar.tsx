9933d1da0c93e7e354ab758fe3a004f2af4d4525
ทนงศักดิ์ แสงศักดิ์ <aomsinsaengsak@gmail.com>
2024-02-16 01:41:25 +0700

    เพิ่มการ logout ที่ NavBar

--- web-client/src/components/Navbar.tsx
+++ web-client/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,14 +7,42 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigate } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const settings = ['Profile', 'Logout'];
+
+const getUser = () => {
+  const User = localStorage.getItem("user") || "";
+  if (User) {
+    return JSON.parse(User);
+  }
+  return false;
+};
 
 export default function Navbar() {
-
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const navigate = useNavigate();
+  const userData = getUser();
 
   const handleLoginClick = () => {
     navigate('/login');
   };
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const Logout = () => {
+      localStorage.removeItem('user')
+      navigate('/')
+  }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -31,10 +60,51 @@ export default function Navbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Godzilla
           </Typography>
-          <Button sx={{ mr: 3}} color="inherit" onClick={handleLoginClick}>
-            Login
-          </Button>
-          <MenuIcon />
+
+          <Typography variant="body2" sx={{ mr: 1, color: 'white' }}>
+            {userData.username}
+          </Typography>
+
+          {userData ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Profile">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                  {settings.map((setting) => (
+                    <MenuItem
+                      key={setting}
+                      onClick={setting === 'Logout' ? Logout : handleCloseUserMenu}
+                      sx={{ mr: 2 }}
+                    >
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                    
+                  ))}
+              </Menu>
+            </Box>
+          ) : (
+            <Button sx={{ mr: 3 }} color="inherit" onClick={handleLoginClick}>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
